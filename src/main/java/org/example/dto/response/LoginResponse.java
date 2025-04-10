a964ec8d448c22471ca6c764aeac284e1cd30e6b
darrenmahh <2829975005@qq.com>
2025-04-10 09:11:46 +0800

    为登录成功返回的请求封装类添加注释

--- src/main/java/org/example/dto/response/LoginResponse.java
+++ src/main/java/org/example/dto/response/LoginResponse.java
@@ -4,9 +4,12 @@ import lombok.Data;
 
 import java.time.LocalDate;
 
+// 登陆成功之后系统给的凭证
 @Data
 public class LoginResponse {
     private String username;
+    // 登录成功之后给的令牌
     private String accessToken;
+    // 令牌过期时间
     private LocalDate expireTime; // xxxx/xx/xx xx:xx:xx
 }
